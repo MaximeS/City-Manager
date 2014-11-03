9d63dba8d17b25f51c9d4db124d3a6e26485f04c
Hugo Couderc <hugo.couderc@hotmail.fr>
2014-11-03 10:27:08 +0100

    pages iscription/connexion

---
+++ pages/Connexion.php
@@ -0,0 +1,15 @@
+<meta charset="UTF-8">
+
+<form method="post" action="CheckConnexion.php">
+	<fieldset>
+	<legend><h2>Connexion</h2></legend>
+	<p>
+	<label for="pseudo">Pseudo :</label> </br> <input name="pseudo" type="text" id="pseudo" /></br> </br>
+	<label for="password">Mot de Passe :</label> </br> <input type="password" name="password" id="password" />
+	</p>
+	</br>
+	<p><input type="submit" value="Connexion" /></p></form>
+	
+	<a href="inscription.php"> pas encore membre ? </a>
+	
+	</fieldset>
---
+++ pages/Inscription.php
@@ -0,0 +1,20 @@
+<meta charset="UTF-8">
+
+<form method="post" action="index.php">
+	<fieldset>
+	<legend><h2>Inscription</h2></legend>
+	<p>
+	<label for="pseudo">Pseudo :</label> </br> <input name="pseudo" type="text" id="pseudo" /></br> </br>
+	
+	<label for="password">Mot de Passe :</label> </br> <input type="password" name="password" id="password" /></br>
+	<label for="password">Vérifier :</label> </br> <input type="password" name="passcheck" id="passcheck" /></br></br>
+	
+	<label for="password">Adresse mail :</label> </br> <input type="text" name="mail_adress" id="mail_adress" /></br>
+	
+	</p>
+	</br>
+	<p><input type="submit" value="Inscription" /></p></form>
+	
+	<a href="Connexion.php"> déja membre ? </a>
+	
+	</fieldset>
