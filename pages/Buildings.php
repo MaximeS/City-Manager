cec851c81d9492acf4ae25528cfd975a6d4c210a
Hugo Couderc <hugo.couderc@hotmail.fr>
2014-11-03 12:06:53 +0100

    buildings +sexy

--- pages/Buildings.php
+++ pages/Buildings.php
@@ -4,11 +4,13 @@
 
 <legend><h2>Batiments</h2></legend>
 
-<div id="Factory_lvl" class="building_bloc"> <h3> Usine </h3> niv :   <button> Améliorer ! </button></div></br>
+<fieldset> <div id="Factory_lvl" class="building_bloc"> <legend> <h3> Usines </h3> </legend> niv :   <button> Améliorer ! </button></div></br> </fieldset>
 
-<div id="Barrack_lvl" class="building_bloc"> <h3> Caserne </h3> niv :   <button> Améliorer ! </button></div> </br>
+<fieldset> <div id="Barrack_lvl" class="building_bloc"> <legend> <h3> Casernes </h3> </legend> niv :   <button> Améliorer ! </button></div> </br> </fieldset>
 
-<div id="Walls_lvl" class="building_bloc"> <h3> Remparts </h3> niv :   <button> Améliorer ! </button></div>  </br>
+<fieldset> <div id="Walls_lvl" class="building_bloc"> <legend> <h3> Remparts </h3> </legend> niv :   <button> Améliorer ! </button></div>  </br> </fieldset>
+
+
 
 
 </fieldset>
