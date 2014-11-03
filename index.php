38e8b1df9158817f7c26de03494d85d5c5a3cf97
MaximeS <maxime.soubiran@gmail.com>
2014-11-03 12:07:22 +0100

    index modification general.css creation

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'lib/header.php';?>
+
+<div id="news_mail"></div>
+<div class="row">
+	<div class="col-md-3"></div>
+	<div class="col-md-6"></div>
+	<div class="col-md-3"></div>
+</div>
+
+
+
+<?php include 'lib/footer.php';?>
---
+++ lib/header.php
@@ -0,0 +1,8 @@
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/general.css">
+
+</head>
+<body>
+	<header><div id="login">Login</div></header>
