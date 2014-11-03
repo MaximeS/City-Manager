71aa5a97c89965c9a89ed286ad24f576615b0a41
Hugo Couderc <hugo.couderc@hotmail.fr>
2014-11-03 11:44:09 +0100

    connexion, iscription, div buildings

---
+++ pages/Buildings.php
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+
+<fieldset>
+
+<legend><h2>Batiments</h2></legend>
+
+<div id="Factory_lvl" class="building_bloc"> <h3> Usine </h3> niv :   <button> Améliorer ! </button></div></br>
+
+<div id="Barrack_lvl" class="building_bloc"> <h3> Caserne </h3> niv :   <button> Améliorer ! </button></div> </br>
+
+<div id="Walls_lvl" class="building_bloc"> <h3> Remparts </h3> niv :   <button> Améliorer ! </button></div>  </br>
+
+
+</fieldset>
