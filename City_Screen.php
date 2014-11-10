55a8a7c8dc514425b1122f40d8d86136e4ef7703
Lothaire Prince <lothaire.prince@y-nov.com>
2014-11-10 13:17:10 +0100

    Modif City_Screen & add City_Screen View

---
+++ City_Screen.php
@@ -0,0 +1,7 @@
+<?php
+
+include '/page/City_Screen'
+
+ici sera inséré la vue -> uniquement la vue et affichera via img src suivant si les fonction seront true or false
+
+?>
--- pages/City_Screen.php
+++ pages/City_Screen.php
@@ -1,4 +1,7 @@
 <?php
+/*
+
+	include '../include/connexion_PDO'
 
 	$stockfactory = $connexion ->prepare("SELECT Factory FROM city WHERE City_id = '%city_id%' ");
 	$sqlfactory = $stockfactory -> execute ();
@@ -28,4 +31,48 @@
 	{
 		echo "<img src="../image/Spy_Barracks.jpg"/>"
 	}
+*/
+	include '../include/connexion_PDO'
+	
+	function Display_Factory
+	{
+		$stockfactory = $connexion ->prepare("SELECT Factory FROM city WHERE City_id = '%city_id%' ");
+		$sqlfactory = $stockfactory -> execute ();
+		
+		if ($sqlfactory > 0)
+		{
+			echo "<img src="../image/Factory.jpg"/>"
+		}
+	}
+	
+	function Display_Barracks
+	{
+		$stockbarracks  = $connexion ->prepare("SELECT Barracks FROM city WHERE City_id = '%city_id%' ");
+		$sqlbarracks = $stockbarracks -> execute ();
+		if ($sqlbarracks > 0)
+		{
+			echo "<img src="../image/Barracks.jpg"/>"
+		}
+	}
+	
+	function Display_Wall
+	{
+		$stockwall = $connexion ->prepare("SELECT Wall FROM city WHERE City_id = '%city_id%' ");
+		$sqlwall = $stockwall -> execute ();
+		if ($sqlwall > 0)
+		{
+			echo "<img src="../image/Wall.jpg"/>"
+		}
+	}
+	
+	function Display_Spy_Barracks
+	{
+		$stockspybarracks = $connexion ->prepare("SELECT Spy_Barracks FROM city WHERE City_id = '%city_id%' ");
+		$sqlspybarracks = $stockspybarracks -> execute ();
+		if ($sqlspybarracks > 0)
+		{
+			echo "<img src="../image/Spy_Barracks.jpg"/>"
+		}
+	}
+	
 ?>
