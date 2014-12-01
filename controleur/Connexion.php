99e95a4f9e974679c094985fb176bdcc505ac3af
MaximeS <maxime.soubiran@gmail.com>
2014-12-01 12:15:22 +0100

    Bootstrap begins

--- controleur/Connexion.php
+++ controleur/Connexion.php
@@ -4,22 +4,25 @@ Session_destroy();
 ?>
 
 <meta charset="UTF-8">
-
-<form name="form_connex" method="post" >
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+<form name="form_connex" method="post" class="form-horizontal">
 	<fieldset>
-	<legend><h2>Connexion</h2></legend>
-	<p>
-	<label for="pseudo">Pseudo :</label> </br> <input name="pseudo" type="text" id="pseudo" /></br> </br>
-	<label for="password">Mot de Passe :</label> </br> <input type="password" name="password" id="password" />
-	</p>
-	</br>
-	<p><input type="submit" value="Connexion" /></p></form>
+		<legend><h2>Connexion</h2></legend>
+		<p>
+			<div class="row">
+				<label for="pseudo" class="col-sm-2 control-label">Pseudo :</label> </br> <input name="pseudo" type="text" id="pseudo" /></br> </br>
+				<label for="password" class="col-sm-2 control-label">Mot de Passe :</label> </br> <input type="password" name="password" id="password" />
+			</p>
+		</br>
+		<p><input type="submit" value="Connexion" class="col-sm-offset-2 btn btn-default"/></p></form>
+		<br/>
+		<a href="inscription.php"><input type="button" class="col-sm-offset-2 btn btn-default" value="pas encore membre ?">  </a>
+	</div>
 	
-	<a href="inscription.php"> pas encore membre ? </a>
 	
-	</fieldset>
-	
-	
+</fieldset>
+
+
 <?php
 
 if(!empty($_POST['pseudo']) AND (!empty($_POST['password'])))
@@ -28,28 +31,28 @@ if(!empty($_POST['pseudo']) AND (!empty($_POST['password'])))
 	$pseudo = $_POST['pseudo'];
 	$pass = $_POST['password'];
 	
-			include 'connexion_PDO.php';
+	include 'connexion_PDO.php';
 
-		$sth = $connexion->prepare("SELECT User_id FROM user WHERE Pseudo = '$pseudo' AND Password = md5('$pass') ");
+	$sth = $connexion->prepare("SELECT User_id FROM user WHERE Pseudo = '$pseudo' AND Password = md5('$pass') ");
 	
-		$sth->execute();
-			
-			$result = $sth->fetchAll();
-			
-			$count = count($result);
-			
-			
+	$sth->execute();
+
+	$result = $sth->fetchAll();
+
+	$count = count($result);
+
+
 	if ($count > 0){
-			
-			
-			
-			echo " <div id='hidden'> <form id='form_login' method='post' action ='../index.php'> <input type='text' name='Pseudo' value=$pseudo> ";
-			echo " <script> document.getElementById('form_login').submit() </script>";
-			
-			
-			
-			
-		}
+
+
+
+		echo " <div id='hidden'> <form id='form_login' method='post' action ='../index.php'> <input type='text' name='Pseudo' value=$pseudo> ";
+		echo " <script> document.getElementById('form_login').submit() </script>";
+
+
+
+
+	}
 	
 	
 }
