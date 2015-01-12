f4dba53829bd9182d79fa28ab9cc9f5f9dbd44b5
Salles-Menjou Cyril <cyril.salles-menjou@y-nov.com>
2015-01-12 09:46:31 +0100

    Cyril

--- vue/lib/headernoco.php
+++ vue/lib/headernoco.php
@@ -1,5 +1,5 @@
 <div color ="#FFFFFF" height="30px" background="#000000"> CECI EST UN HEADER ! </div>
 
-<a href="/SlimTry/"> retour a l'acceuil </a>
+<a href="/City-Manager/"> retour a l'acceuil </a>
 
 </br> </br>
