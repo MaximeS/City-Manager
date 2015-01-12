b86c022402bc0ecfd3e34e32b09833149f7d42d5
MaximeS <maxime.soubiran@gmail.com>
2015-01-12 11:26:58 +0100

    Add profile view

--- vue/lib/profil.php
+++ vue/lib/profil.php
@@ -1,1 +1,13 @@
 <h1> Vous êtes sur le profil de <?php echo $Pseudo ?> ! </h1>
+
+<?php
+foreach ($cities as $city) {
+	echo "<div>".$city['Name']."<br>"
+	.$city['Gold_S']."<br>"
+	.$city['Soldier_S']."<br>"
+	."<form method="POST" action='/City-Manager/".$_SESSION['Player_Pseudo']."'>
+	<input type="hidden" value='".$city['City_id']."' name="city_id">
+	<input type="submit" value="Voir"> "
+
+	}
+;?>
